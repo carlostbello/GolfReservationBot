ef42f12fee1d6278de2189d6762bbecf051e5b34
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2020-12-12 21:19:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from numpy import loadtxt
 import schedule
 import time
 
+# Retrieve webdriver via Selenium
 browser = webdriver.Chrome('D:\Programming Projects\Project 1\chromedriver')
 browser.get('http://staelenagolf.com/default.aspx')
 
+# Load in the info to be filled ex. FName, LName, Member ID, email, etc.
 lines = loadtxt("Contents.txt", dtype='str')
 
 nameBar = browser.find_element_by_id("MainContent_txtFirstName")
@@ -27,17 +29,20 @@ nameBar = browser.find_element_by_id("MainContent_txtNumCarts")
 nameBar.send_keys(lines[5])
 
 nextButtonP1 = browser.find_element_by_id("MainContent_btnNext")
-
+# Time trigger, as it is first come first serve
 schedule.every().monday.at('12:00').do(nextButtonP1.click())
 
 # Date Path dependent on the given calendar date
-dateButton = browser.find_element_by_xpath('//*[@id="MainContent_calendar"]/div/table/tbody/tr[3]/td[1]/a')
+dateRow = str(3)
+dateColumn = str(1)
+dateButton = browser.find_element_by_xpath('//*[@id="MainContent_calendar"]/div/table/tbody/tr[' + dateRow + ']/td[' + dateColumn + ']/a')
 dateButton.click()
 
-i = 4
+# Time Selector. Cycle between 4 time slots between 6:45 and 7:15 
+i = 4 # i = 4 because the 4th time slot is 6:45
 stayLoop = True
 timeButton = browser.find_elements_by_xpath('//*[@id="MainContent_c1_lvTeetimes_grpTemplate"]/li[' + str(i) + ']/a')
-while stayLoop and i <= 3:
+while stayLoop and i <= 3: # The increments are by 15 mins
     timeButton = browser.find_elements_by_xpath('//*[@id="MainContent_c1_lvTeetimes_grpTemplate"]/li[' + str(i) + ']/a')
     if timeButton.get_attribute("title") == "Taken":
         i += 1
